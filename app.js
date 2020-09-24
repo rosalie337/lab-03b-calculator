a4d48212ab0789d612ee5a54edb3a514fec3886a
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-09-24 14:25:03 -0400

    added sub event listener console log

--- app.js
+++ app.js
@@ -4,28 +4,37 @@ const addInput2 =document.getElementById('add-input-2')
 const addButton =document.getElementById('add-button')
 const addResults =document.getElementById('add-results')
 
+// initialize state
+// set event listeners to update state and DOM
+addButton.addEventListener('click', () => {
+    
+    const value1 = addInput1.value;
+    const value2 = addInput2.value;
+    
+    console.log(value1, value2)
+    
+    const value1AsNumber = Number(value1);
+    const value2AsNumber = Number(value2);
+    
+    console.log(typeof value1AsNumber, typeof value2AsNumber);
+    
+    const sum = value1AsNumber + value2AsNumber;
+    
+    console.log(sum);
+    
+    addResults.textContent = sum;
+});
+
 const subtractionInput1 =document.getElementById('subtraction-input-1')
 const subtractionInput2 =document.getElementById('subtraction-input-2')
 const subtractionButton =document.getElementById('subtraction-button')
 const subtractionResults =document.getElementById('subtraction-results')
 
-// initialize state
-// set event listeners to update state and DOM
-addButton.addEventListener('click', () => {
- 
-    const value1 = addInput1.value;
-    const value2 = addInput2.value;
+subtractionButton.addEventListener('click', () => {
 
-    console.log(value1, value2)
+    const subValue1 = subtractionInput1.value;
+    const subValue2 = subtractionInput2.value;
 
-    const value1AsNumber = Number(value1);
-    const value2AsNumber = Number(value2);
+    console.log(subValue1, subValue2)
 
-    console.log(typeof value1AsNumber, typeof value2AsNumber);
-
-    const sum = value1AsNumber + value2AsNumber;
-    
-    console.log(sum);
-
-    addResults.textContent = sum;
 });
