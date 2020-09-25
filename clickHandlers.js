6972525b5d748834cb6c9a4ff513eba57c4bc053
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-09-25 17:16:25 -0400

    NaN is the current response when calculating numbers

--- clickHandlers.js
+++ clickHandlers.js
@@ -4,7 +4,7 @@ const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addResults = document.getElementById('add-results');
 
-export function handAddClick () {
+export function handleAddClick () {
     const value1 = addInput1.value1AsNumber;
     const value2 = addInput2.value2AsNumber;
     
